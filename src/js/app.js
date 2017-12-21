9d05de8020f287a4a537f76f5923c9678ae9cb16
Loïc Hamet <loic.hamet@gmail.com>
2017-12-22 00:13:19 +0100

    init react

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
-console.log('app start');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div id="app">
+        App
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app-container'));
